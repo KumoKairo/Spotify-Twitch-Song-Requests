26533515255de08c8979ca66d3f9f8ac1132dcc1
AdamMcD94 <36520109+AdamMcD94@users.noreply.github.com>
2022-03-12 13:46:05 +0000

    Single Quotes

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const chatbotConfig = setupYamlConfigs();
 
 const expressPort = chatbotConfig.express_port;
 
-let spotifyRefreshToken = "";
-let spotifyAccessToken = "";
+let spotifyRefreshToken = '';
+let spotifyAccessToken = '';
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const twitchOauthToken = process.env.TWITCH_OAUTH_TOKEN;
 
-const channelPointsUsageType = "channel_points";
-const commandUsageType = "command";
+const channelPointsUsageType = 'channel_points';
+const commandUsageType = 'command';
 
 if(chatbotConfig.usage_type !== channelPointsUsageType && chatbotConfig.usage_type !== commandUsageType) {
     console.log(`Usage type is neither "${channelPointsUsageType}" nor "${commandUsageType}", app will not work. Edit your settings in the 'spotipack_config.yaml' file`);
@@ -45,17 +45,17 @@ client.connect().catch(console.error);
 
 console.log(`Logged in as ${chatbotConfig.user_name}. Working on channel "${chatbotConfig.channel_name}"`);
 
-client.on("message", async (channel, tags, message, self) => {
+client.on('message', async (channel, tags, message, self) => {
     if(self) return;
 
     let messageToLower = message.toLowerCase();
 
-    if(chatbotConfig.usage_type === commandUsageType && messageToLower.startsWith("!songrequest")) {
+    if(chatbotConfig.usage_type === commandUsageType && messageToLower.startsWith('!songrequest')) {
         let result = await handleSongRequest(channel, tags, message, true);
         if(!result) {
             client.say(chatbotConfig.channel_name, chatbotConfig.song_not_found);
         }
-    } else if (chatbotConfig.use_song_command && messageToLower === "!song") {
+    } else if (chatbotConfig.use_song_command && messageToLower === '!song') {
         await handleTrackName(channel);
     }
 });
@@ -81,7 +81,7 @@ let handleTrackName = async (channel) => {
             await refreshAccessToken();
             await printTrackName(channel);
         } else {
-            client.say(chatbotConfig.channel_name, `Seems like no music is playing right now`);
+            client.say(chatbotConfig.channel_name, 'Seems like no music is playing right now');
         }
     }
 }
@@ -89,14 +89,14 @@ let handleTrackName = async (channel) => {
 let printTrackName = async (channel) => {
     let spotifyHeaders = getSpotifyHeaders();
 
-    let res = await axios.get(`https://api.spotify.com/v1/me/player/currently-playing`, {
+    let res = await axios.get('https://api.spotify.com/v1/me/player/currently-playing', {
         headers: spotifyHeaders
     });
 
     let trackId = res.data.item.id;
     let trackInfo = await getTrackInfo(trackId);
     let trackName = trackInfo.name;
-    let artists = trackInfo.artists.map(artist => artist.name).join(", ");
+    let artists = trackInfo.artists.map(artist => artist.name).join(', ');
     client.say(channel, `${artists} - ${trackName}`);
 }
 
@@ -130,13 +130,13 @@ let searchTrackID = async (searchString) => {
 }
 
 let validateSongRequest = async (message, channel, tags, runAsCommand) => {
-    let url = "", searchString = "";
+    let url = '', searchString = '';
     let usernameParams = {
         username: tags.username
     };
 
     if(runAsCommand) {
-        let splitMessage = message.split(" ");
+        let splitMessage = message.split(' ');
 
         if (splitMessage.length < 2) {
             client.say(channel, handleMessageQueries(chatbotConfig.usage_message, usernameParams));
@@ -145,7 +145,7 @@ let validateSongRequest = async (message, channel, tags, runAsCommand) => {
 
         url = splitMessage[1];
         splitMessage.shift();
-        searchString = splitMessage.join(" ");
+        searchString = splitMessage.join(' ');
     } else {
         url = message;
         searchString = message;
@@ -176,7 +176,7 @@ let addSongToQueue = async (songId, channel) => {
     let trackInfo = await getTrackInfo(songId);
 
     let trackName = trackInfo.name;
-    let artists = trackInfo.artists.map(artist => artist.name).join(", ");
+    let artists = trackInfo.artists.map(artist => artist.name).join(', ');
 
     let uri = trackInfo.uri;
 
@@ -193,11 +193,11 @@ let addSongToQueue = async (songId, channel) => {
 let refreshAccessToken = async () => {
     const params = new URLSearchParams();
     params.append('refresh_token', spotifyRefreshToken);
-    params.append('grant_type', "refresh_token");
+    params.append('grant_type', 'refresh_token');
     params.append('redirect_uri', `http://localhost:${expressPort}/callback`);
 
     try {
-        let res = await axios.post(`https://accounts.spotify.com/api/token`, params, {
+        let res = await axios.post('https://accounts.spotify.com/api/token', params, {
             headers: {
                 'Content-Type':'application/x-www-form-urlencoded',
                 'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
@@ -258,7 +258,7 @@ app.get('/callback', async (req, res) => {
     spotifyAccessToken = tokenResponse.data.access_token;
     spotifyRefreshToken = tokenResponse.data.refresh_token;
 
-    res.send("Tokens refreshed successfully. You can close this tab");
+    res.send('Tokens refreshed successfully. You can close this tab');
 });
 
 app.listen(expressPort);
