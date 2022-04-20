b90bcf478968eeecbccff51e5bfd4aaf29545808
ghlegend195 <ghlegend195@aol.com>
2022-04-19 21:59:20 -0700

    Revert "Fixed Issue #4, properly this time"
    This reverts commit 8338343c7136016f01ce5f84138f7938c5ec3705.

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ let getTrackInfo = async (trackId) => {
     return trackInfo.data;
 }
 
-let addSongToQueue = async (songId, channel, username) => {
+let addSongToQueue = async (songId, channel) => {
     let spotifyHeaders = getSpotifyHeaders();
 
     let trackInfo = await getTrackInfo(songId);
